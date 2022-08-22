2c28df207e1f41b508ad8ad73a4de604dc0177d8
Muskan Katariya <64361475+tech-girl123@users.noreply.github.com>
2022-08-22 00:21:03 -0400

    Add files via upload

---
+++ assign1_tf-main/terraform_code 3/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Muskan"
+    "App"   = "Web"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value = "week1"
+}
